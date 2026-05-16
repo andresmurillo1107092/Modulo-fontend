9caea4f7d2aae3a813c6a954e8c913c9d2f44b6c
Andres Felipe Murillo <andresmurillo1107092@gmail.com>
2026-05-16 10:47:12 -0500

    sexto commit

--- clase_1/util.js
+++ clase_1/util.js
@@ -13,3 +13,18 @@ console.log("La igualdad es: " + (parametro1 == parametro2));
 console.log("La desigualdad es: " + (parametro1 != parametro2));
 console.log("La igualdad estricta es: " + (parametro1 === parametro2));
 console.log("La desigualdad estricta es: " + (parametro1 !== parametro2));  
+
+console.log("el parametro1 es:" + (parametro1 > parametro2 ? "mayor" : "menor"));
+console.log("el parametro2 es:" + (parametro2 < parametro1 ? "mayor" : "menor"));
+console.log("el parametro1 es:" + (parametro1 >= parametro2 ? "mayor o igual" : "menor"));
+console.log("el parametro2 es:" + (parametro2 <= parametro1 ? "mayor o igual" : "menor"));
+console.log("el parametro1 es:" + (parametro1 == parametro2 ? "igual" : "diferente"));
+console.log("el parametro2 es:" + (parametro2 === parametro1 ? "igual" : "diferente"));
+console.log("el parametro1 es:" + (parametro1 != parametro2 ? "diferente" : "igual"));
+console.log("el parametro2 es:" + (parametro2 !== parametro1 ? "diferente" : "igual"));
+
+let edad = 25;
+
+console.log("La edad es:" + (edad > 18 && edad < 60 ? "Es mayor de edad y menor de 65 años" : "No es mayor de edad o es mayor de 60 años"));
+console.log("La edad es:" + (edad < 18 || edad > 60 ? "No es mayor de edad o es mayor de 60 años" : "Es mayor de edad y menor de 65 años"));
+console.log("La edad es:" + (!(edad < 18) ? "Es mayor de edad y menor de 65 años" : "No es mayor de edad o es mayor de 60 años"));
