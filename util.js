43a154df100b3c8433bcb26b8618cbbae97edbcf
Andres Felipe Murillo <andresmurillo1107092@gmail.com>
2026-05-16 09:30:38 -0500

    cuarto commit

---
+++ util.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
+//let variable 1 = valor
+//var
+//const
+
+let numero =5;
+var decimal = 4.5;
+const pi = 3.1416;
+let texto ="texto"
+let booleanos = true;
+let arreglo ={};
+let objeto ={
+    nombre ="juan",
+    edad ="30",
+    profesion ="ingeniero industrial",
+
+};
+})
+// comentario 1 linea
+/*
+comentarios de varias lineas
+*/
+
+function saludar(nombre ="mundo")
+{
+console.log("Hola, " + nombre+ "!");
+};
+
+saludar();
+
+//segunda funcion   
+let parametro1 = 10;
+let parametro2 = 20;
+
+console.log("La suma es: " + (parametro1 + parametro2));
+console.log("La resta es: " + (parametro1 - parametro2));
+console.log("La multiplicacion es: " + (parametro1 * parametro2));
+console.log("La division es: " + (parametro1 / parametro2));
+console.log("El modulo es: " + (parametro1 % parametro2));
+console.log("La potencia es: " + (parametro1 ** parametro2));
+console.log("El incremento es: " + (++parametro1));
+console.log("El decremento es: " + (--parametro2));
+console.log("La igualdad es: " + (parametro1 == parametro2));
+console.log("La desigualdad es: " + (parametro1 != parametro2));
+console.log("La igualdad estricta es: " + (parametro1 === parametro2));
+console.log("La desigualdad estricta es: " + (parametro1 !== parametro2));  
+
