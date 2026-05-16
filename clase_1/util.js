ad519cf70a73b11f9534be70e72b42e14cf5102f
Andres Felipe Murillo <andresmurillo1107092@gmail.com>
2026-05-16 09:42:31 -0500

    quinto commit

--- clase_1/util.js
+++ clase_1/util.js
@@ -1,36 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(){
-
-
-//let variable 1 = valor
-//var
-//const
-
-let numero =5;
-var decimal = 4.5;
-const pi = 3.1416;
-let texto ="texto"
-let booleanos = true;
-let arreglo ={};
-let objeto ={
-    nombre ="juan",
-    edad ="30",
-    profesion ="ingeniero industrial",
-
-};
-})
-// comentario 1 linea
-/*
-comentarios de varias lineas
-*/
-
-function saludar(nombre ="mundo")
-{
-console.log("Hola, " + nombre+ "!");
-};
-
-saludar();
-
-//segunda funcion   
 let parametro1 = 10;
 let parametro2 = 20;
 
@@ -46,4 +13,3 @@ console.log("La igualdad es: " + (parametro1 == parametro2));
 console.log("La desigualdad es: " + (parametro1 != parametro2));
 console.log("La igualdad estricta es: " + (parametro1 === parametro2));
 console.log("La desigualdad estricta es: " + (parametro1 !== parametro2));  
-
